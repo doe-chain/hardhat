1ad4a07e8ed0dbe269a1836ad4f09f096699d463
smolyar <smolyar@gmail.com>
2022-09-16 13:10:45 +0300

    +

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+require("@hardhat-chai-matchers");
+require("@hardhat-ethers");
+
 module.exports = {
   solidity: "0.8.9",
 };

09866fa64ef03ad87f586d5526c0d0429c645964
smolyar <smolyar@gmail.com>
2022-09-16 19:18:59 +0300

    token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.0;
 import "./Erc.sol";
 
 contract Token is ERC20 {
-    constructor (address shop) ERC20("BaseToken", "BT", 1000, shop) {
-
-    }
+    constructor (address shop) ERC20("BaseToken", "BT", 1000, shop) {}
 
 }

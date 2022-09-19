8339375ce1ee797fcc41d8f0d43fe1f562467a04
smolyar <smolyar@gmail.com>
2022-09-19 23:59:56 +0300

    IERC721

---
+++ contracts/IERC721.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+interface IERC721 {
+    event Transfer(address indexed from, address indexed to, uint indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function balanceOf(address owner) external view returns(uint);
+
+    function ownerOf(uint tokenId) external view returns(address);
+
+    function transferFrom(address from, address to, uint tokenId) external;
+
+    function safeTransferFrom(address from, address to, uint tokenId) external;
+
+    //realization like in OZ - same name but different arguments count, "data" for message or smth
+    function safeTransferFrom(address from, address to, uint tokenId, bytes calldata data) external;
+
+    function approve(address to, uint tokenId) external;
+
+    function setApprovalForAll(address operator, bool approved) external;
+
+    function getApproved(uint tokenId) external view returns(address);
+
+    function getApprovedForAll(address owner, address operator) external view returns(bool);
+} 

70193a8f361b7e783f011e89b3375ca02bc3e3ae
smolyar <smolyar@gmail.com>
2022-09-19 15:10:04 +0300

    tests passed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
-//require("@hardhat-chai-matchers");
-//require("@hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.10",
--- test/shop-test.js
+++ test/shop-test.js
@@ -1,11 +1,11 @@
 const {expect} = require("chai")
 const{ethers} = require("hardhat")
-const tokenJSON = require("../artifacts/contracts/Token.sol/Shop.json")
+const tokenJSON = require("../artifacts/contracts/Erc.sol/ERC20.json")
 
 describe("Shop", function() {
     let owner
     let buyer
-    let Shop
+    let shop
     let erc20
 
     
@@ -13,6 +13,7 @@ describe("Shop", function() {
     beforeEach(async function() {
         [owner, buyer] = await ethers.getSigners()
 
+        
         const Shop = await ethers.getContractFactory("Shop", owner)
 
         shop = await Shop.deploy()
@@ -23,8 +24,11 @@ describe("Shop", function() {
     })
 
     it("have owner and token", async function() {
-        expect(await shop.owner.to.eq(owner.address))
-
+        // console.log(owner.address)
+        // console.log(shop.owner)
+        // console.dir(shop)
+        expect(await shop.owner()).to.eq(owner.address)
+        
         expect(await shop.token()).to.be.properAddress
     })
 
@@ -38,7 +42,7 @@ describe("Shop", function() {
         const tx = await buyer.sendTransaction(txData)
         await tx.wait()
         
-        expect(await erc20.balanceOf(buyer.address)).to.eq.apply(tokenAmount)
+        expect(await erc20.balanceOf(buyer.address)).to.eq(tokenAmount)
 
         await expect(() => tx).to.changeEtherBalance(shop, tokenAmount)
 
@@ -48,13 +52,13 @@ describe("Shop", function() {
     it("allow to buy", async function() {
         const tx = await buyer.sendTransaction({
             value:10,
-            to: Shop.address
+            to: shop.address
         })
         await tx.wait()
 
         const sellAmount = 6
 
-        const approval = await erc20.connect(buyer).approve(Shop.address, sellAmount)
+        const approval = await erc20.connect(buyer).approve(shop.address, sellAmount)
 
         await approval.wait()
 
@@ -63,7 +67,7 @@ describe("Shop", function() {
 
         await expect(() => sellTx).to.changeEtherBalance(shop, -sellAmount)
 
-        await expect(tx).to.emit(shop, "Sold").withArgs(sellAmount, buyer.address)
+        await expect(sellTx).to.emit(shop, "Sold").withArgs(sellAmount, buyer.address)
 
     })
 
